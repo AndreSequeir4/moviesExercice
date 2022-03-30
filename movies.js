6718261884c42a11ad962494b8c8bf158073792f
André <andre.aama.sequeira@gmail.com>
2022-03-30 11:17:42 +0100

    Nearly Done Task Movies´

--- movies.js
+++ movies.js
@@ -1,30 +1,190 @@
-const movies = require('./data.js');
+const movies = require("./data.js");
 
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-const getAllDirectors = () => {};
+
+//1
+
+const getAllDirectors = () => {
+  const directors = movies.map((movie) => {
+    return movie.director;
+  });
+  return directors;
+};
+
+//1(Bonus)
+
+const getAllDirectorsNotDuplicated = () => {
+  const uniqueDirectors = [];
+  movies.forEach((movie) => {
+    if (!uniqueDirectors.includes(movie.director)) {
+      uniqueDirectors.push(movie.director);
+    }
+  });
+  return uniqueDirectors;
+};
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-const getNumberOfDramaMovies = () => {};
+
+//2
+
+const getNumberOfDramaMovies = () => {
+  count = 0;
+  movies.forEach((movie) => {
+    if (
+      movie.director === "Steven Spielberg" &&
+      movie.genre.includes("Drama")
+    ) {
+      count = count + 1;
+    }
+  });
+  return count;
+};
+
+//2 - Filter
+
+function stevenDramaMovies() {
+  let filtred = movies.filter((currentMovie) => {
+    return (
+      currentMovie.director === "Steven Spielberg" &&
+      currentMovie.genre.includes("Drama")
+    );
+  });
+  return filtred.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-const getAverageScore = () => {};
+
+const getAverageScore = () => {
+  let averageScore = 0;
+  const scoreValue = movies
+    .reduce((acc, movie) => acc + movie.score, 0)
+    .toFixed(2);
+  averageScore = scoreValue / movies.length;
+  return averageScore.toFixed(2);
+};
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-const getAverageDramaScore = () => {};
+
+const getAverageDramaScore = () => {
+  let averageDramaScoreValue = 0;
+  let filtered = movies.filter((currentMovie) => {
+    return currentMovie.genre.includes("Drama");
+  });
+  const scoreDramaValue = filtered
+    .reduce((acc, dramaMovie) => acc + dramaMovie.score, 0)
+    .toFixed(2);
+  averageDramaScoreValue = scoreDramaValue / filtered.length;
+  return averageDramaScoreValue.toFixed(2);
+};
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-const getMoviesByYear = () => {};
+
+const getMoviesByYear = () => {
+  const sorted = movies.sort((movieOne, movieTwo) => {
+    const { title: titleOne, year: yearOne } = movieOne;
+    const { title: titleTwo, year: yearTwo } = movieTwo;
+    if (yearOne - yearTwo === 0) {
+      return titleOne.localeCompare(titleTwo);
+    } else {
+      return yearOne - yearTwo;
+    }
+  });
+  return sorted;
+};
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-const getMoviesByTitle = () => {};
+const getMoviesByTitle = () => {
+  const sorted = movies
+    .sort((movieOne, movieTwo) => {
+      return movieOne.title.localeCompare(movieTwo.title);
+    })
+    .slice(0, 20);
+  return sorted;
+};
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-const turnHoursToMinutes = () => {};
+
+const turnHoursToMinutes = () => {
+  const newMovies = movies.map((movie) => {
+    const { duration } = movie;
+    let durationSplit = duration.split(" ");
+    let hour = parseInt(durationSplit[0][0]);
+    let minutes = durationSplit[1];
+    if (!minutes) {
+      minutes = hour * 60;
+    } else {
+      for (let i = 0; i < minutes.length; i++) {
+        if (minutes[i++] === "m") {
+          firstMinute = parseInt(minutes[i]);
+          return `${firstMinute}`;
+        } else {
+          firstMinute = parseInt(minutes[i]);
+          secondMinute = parseInt(minutes[i++]);
+          return `${firstMinute} ${secondMinute}`;
+        }
+      }
+    }
+  });
+  return newMovies;
+};
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-const getBestYearlyScoreAverage = () => {};
+
+const getBestYearlyScoreAverage = () => {
+  let usedYearsArray = [];
+  let highestAverage = 0;
+  const finalObj = {};
+  movies.map((movie) => {
+    if (usedYearsArray.includes(movie.year)) {
+      return;
+    } else {
+      usedYearsArray.push(movie.year);
+      let filtered = movies.filter((movieFilter) => {
+        return movieFilter.year === movie.year;
+      });
+      const yearAverage = filtered.reduce((acc, movie) => acc + movie.score, 0);
+      let average = yearAverage / filtered.length;
+      if (average >= highestAverage) {
+        highestAverage = average;
+
+        return (immutableObj = {
+          ...finalObj,
+          highestAverage,
+          year: movie.year,
+        });
+      }
+    }
+  });
+  return immutableObj;
+};
 
 // BONUS - Iteration 9: Worst yearly score average - Worst yearly score average
-const getWorstYearlyScoreAverage = () => {};
+const getWorstYearlyScoreAverage = () => {
+  let usedYearsArray = [];
+  let lowestAverage = 10;
+  const finalObj = {};
+  movies.map((movie) => {
+    if (usedYearsArray.includes(movie.year)) {
+      return;
+    } else {
+      usedYearsArray.push(movie.year);
+      let filtered = movies.filter((movieFilter) => {
+        return movieFilter.year === movie.year;
+      });
+      const yearAverage = filtered.reduce((acc, movie) => acc + movie.score, 0);
+      let average = yearAverage / filtered.length;
+      if (average <= lowestAverage) {
+        lowestAverage = average;
+
+        return (immutableObj = {
+          ...finalObj,
+          lowestAverage,
+          year: movie.year,
+        });
+      }
+    }
+  });
+  return immutableObj;
+};
